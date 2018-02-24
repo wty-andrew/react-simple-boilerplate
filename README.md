51b76262f9be162c85791a01b713a8e0f842eb8d
andrewty <wty.andrew@gmail.com>
2018-02-24 09:52:53 +0800

    add dev server & README

---
+++ README.md
@@ -0,0 +1,14 @@
+# Quick start
+ 
+``` bash
+npm install
+npm run build
+npm start
+```
+
+# Available Commands
+ 
+- `npm start`: starts the server
+- `npm run dev`: run webpack in watch mode
+- `npm run build`: bundles the code into public folder
+- `npm run lint`: check for lint errors
---
+++ server.js
@@ -0,0 +1,17 @@
+const path = require('path')
+const express = require('express')
+
+const app = express()
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, err => {
+  if (!err) {
+    /* eslint-disable no-console */
+    console.log(`Server is running on port ${PORT}`)
+  }
+})
